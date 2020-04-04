4b7e42422cb599c6f190e2646673827c9fcb9ae5
Joris Coppieters <joris@trademe.co.nz>
2020-04-04 13:30:00 +1300

    Added copied banner

--- src/script.js
+++ src/script.js
@@ -1,23 +1,34 @@
-var removeAllRangesTimeout = null;
+var copiedTimeout = null;
 const getGuid = () => {
     const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 };
 const selectGuid = () => {
-    document.getElementById('guid').select();
-    if (removeAllRangesTimeout) {
-        clearTimeout(removeAllRangesTimeout);
-    }
-    removeAllRangesTimeout = setTimeout(() => {
-        document.getSelection().removeAllRanges();
-    }, 1000)
+    copyGuid();
 };
 const setRandomGuid = () => {
     const guid = getGuid().toUpperCase();
     document.getElementById('guid').value = guid;
     setTimeout(() => {
-        document.getElementById('guid').select();
-        document.execCommand('copy');
-        document.getSelection().removeAllRanges();
+        copyGuid();
     }, 200);
+}
+const copyGuid = () => {
+    document.getElementById('guid').focus();
+    document.getElementById('guid').select();
+    if (!document.execCommand('copy')) {
+        console.error('Failed to copy GUID!');
+    }
+    document.getSelection().removeAllRanges();
+
+    document.getElementById('copied').className = 'show';
+    if (copiedTimeout) {
+        clearTimeout(copiedTimeout);
+    }
+    copiedTimeout = setTimeout(() => {
+        document.getElementById('copied').className = '';
+    }, 1000)
+}
+const init = () => {
+    document.getElementById('guid').value = '<CLICK GENERATE>';
 }

a1c0373f61636d6c7fcac46344395c533d1ba8ae
Joris Coppieters <joris@trademe.co.nz>
2020-07-30 17:28:46 +1200

    conf fixes

--- build/scripts/publish-remote.sh
+++ build/scripts/publish-remote.sh
@@ -8,25 +8,25 @@ SITES_DIR="$ROOT_DIR/sites"; mkdir -p $SITES_DIR
 SITES_BACKUP_DIR="$ROOT_DIR/sites_backup"; mkdir -p $SITES_BACKUP_DIR
 CONF_DIR="$ROOT_DIR/conf"; mkdir -p $CONF_DIR
 
-if [[ -d "$SITES_DIR/<HOST>/" ]]; then
-  mv "$SITES_DIR/<HOST>/" "$SITES_BACKUP_DIR/<CURRENT_DATE_STAMP>_<HOST>"
+if [[ -d "$SITES_DIR/<ENV_HOST>/" ]]; then
+  mv "$SITES_DIR/<ENV_HOST>/" "$SITES_BACKUP_DIR/<CURRENT_DATE_STAMP>_<ENV_HOST>"
 fi
 
-mkdir "$SITES_DIR/<HOST>"
-cd "$SITES_DIR/<HOST>"
+mkdir "$SITES_DIR/<ENV_HOST>"
+cd "$SITES_DIR/<ENV_HOST>"
 
 unzip "$DOWNLOADS_DIR/<DIST_ZIP>"
 sudo chown -R www-data:www-data .
 sudo chown www-data:ubuntu .
 
 cd "$CONF_DIR"
-rm -f unlink "<HOST>.conf"
-mv "$SITES_DIR/<HOST>/http.conf" "<HOST>.conf"
+rm -f unlink "<ENV_HOST>.conf"
+mv "$SITES_DIR/<ENV_HOST>/http.conf" "<ENV_HOST>.conf"
 
-rm -f "<HOST>-le-ssl.conf"
-mv "$SITES_DIR/<HOST>/https.conf" "<HOST>-le-ssl.conf"
+rm -f "<ENV_HOST>-le-ssl.conf"
+mv "$SITES_DIR/<ENV_HOST>/https.conf" "<ENV_HOST>-le-ssl.conf"
 
-sudo a2ensite "<HOST>.conf" "<HOST>-le-ssl.conf"
+sudo a2ensite "<ENV_HOST>.conf" "<ENV_HOST>-le-ssl.conf"
 sudo systemctl reload apache2
 
 rm "$DOWNLOADS_DIR/<DIST_ZIP>"
--- build/scripts/publish.sh
+++ build/scripts/publish.sh
@@ -5,23 +5,42 @@ set -x # Print out all commands
 HOST=$npm_package_config_host
 if [[ ! $HOST ]]; then echo "npm_package_config_host isn't set!"; exit -1; fi
 
-ENV=$1
-if [[ ! $ENV ]]; then
-    ENV="prod"
+VERSION=$npm_package_version
+if [[ ! $VERSION ]]; then echo "npm_package_version isn't set!"; exit -1; fi
+
+NAME=$npm_package_name
+if [[ ! $NAME ]]; then echo "npm_package_name isn't set!"; exit -1; fi
+
+PORT_IDX=$npm_package_config_port_idx
+if [[ ! $PORT_IDX ]]; then echo "npm_package_config_port_idx isn't set!"; exit -1; fi
+
+ENV_TYPE=$1
+if [[ ! $ENV_TYPE ]]; then
+    ENV_TYPE="prod"
 fi
 
-if [[ $ENV == "test" ]]; then
-    HOST="test.$HOST"
+ENV_TYPE_FULL="Production"
+if [[ $ENV_TYPE == "test" ]]; then
+    ENV_TYPE_FULL="Test"
+elif [[ $ENV_TYPE == "dev" ]]; then
+    ENV_TYPE_FULL="Development"
+fi
+
+ENV_HOST=$HOST
+if [[ $ENV_TYPE == "test" ]]; then
+    ENV_HOST="test.$ENV_HOST"
+elif [[ $ENV_TYPE == "dev" ]]; then
+    ENV_HOST="dev.$ENV_HOST"
 fi
 
 CURRENT_DATE_STAMP=`date +"%Y-%m-%d-%H%M%S"`
 
 SERVER_ADMIN="jobot.software@gmail.com"
-DIST_ZIP="dist-$HOST-$CURRENT_DATE_STAMP.zip"
-REMOTE_SCRIPT="deploy-$HOST-$CURRENT_DATE_STAMP.sh"
+DIST_ZIP="dist-$ENV_HOST-$CURRENT_DATE_STAMP.zip"
+REMOTE_SCRIPT="deploy-$ENV_HOST-$CURRENT_DATE_STAMP.sh"
 
 WEBPACK_CONFIG="prod"
-if [[ $ENV == "dev" ]]; then
+if [[ $ENV_TYPE == "dev" ]]; then
     WEBPACK_CONFIG="dev"
 fi
 
@@ -32,12 +51,19 @@ function replace_vars () {
     fi
     FILE=$1
     sed -i '
-        s/<ENV>/'$ENV'/g;
-        s/<HOST>/'$HOST'/g;
+        s/<APP_VERSION>/'$VERSION'/g;
+        s/<APP_NAME>/'$NAME'/g;
+        s/<APP_HOST>/'$HOST'/g;
+        s/<APP_PORT_IDX>/'$PORT_IDX'/g;
+        s/<APP_ENV_TYPE>/'$ENV_TYPE_FULL'/g;
+        s/<ENV_HOST>/'$ENV_HOST'/g;
+        s/<ENV_TYPE>/'$ENV_TYPE'/g;
+        s/<ENV_TYPE_FULL>/'$ENV_TYPE_FULL'/g;
         s/<CURRENT_DATE_STAMP>/'$CURRENT_DATE_STAMP'/g;
         s/<SERVER_ADMIN>/'$SERVER_ADMIN'/g;
         s/<DIST_ZIP>/'$DIST_ZIP'/g;
         s/<REMOTE_SCRIPT>/'$REMOTE_SCRIPT'/g;
+        s/<SOCKET_HTTP_PORT>/'$SOCKET_HTTP_PORT'/g;
     ' $FILE
 }
 
@@ -60,7 +86,7 @@ echo "#"
 echo ""
 set -x
 
-webpack_env=$ENV webpack --config build/webpack/site/webpack.$WEBPACK_CONFIG.js
+webpack_env=$ENV_TYPE webpack --config build/webpack/site/webpack.$WEBPACK_CONFIG.js
 cp -r \
     ./build/scripts/apache2/http.conf \
     ./build/scripts/apache2/https.conf \
@@ -69,7 +95,8 @@ cp -r \
 replace_vars ./dist/site/http.conf
 replace_vars ./dist/site/https.conf
 
-if [[ $ENV != "dev" ]]; then
+if [[ $ENV_TYPE != "dev" ]]; then
+
     set +x
     echo ""
     echo "#"

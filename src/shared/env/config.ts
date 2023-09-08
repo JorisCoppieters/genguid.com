bf7e2985d8a64737689e76911846f7dda0508707
JorisCoppieters <joris.coppieters@gmail.com>
2023-09-08 16:49:34 +1200

    removed site check and e2e tests

--- src/shared/env/config.ts
+++ src/shared/env/config.ts
@@ -2,7 +2,7 @@ import { config as baseConfig } from './config.base';
 import { ENV_TYPE } from '../enums/env-type';
 
 export const config = Object.assign(baseConfig, {
-    envType: ENV_TYPE.Development,
+    envType: ENV_TYPE.Test,
     r18: false,
     requireEmailConfirmation: true,
 });
